77823eaa89f7200510e49b5ff4edc3a17b402cf1
muscharling <55578290+muscharling@users.noreply.github.com>
2020-05-17 13:05:11 +0200

    ny js fil

--- js/jewel-it.js
+++ js/jewel-it.js
@@ -22,3 +22,23 @@
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+//formvalidering
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
